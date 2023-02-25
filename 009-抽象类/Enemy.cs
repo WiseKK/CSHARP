fa50f9cfb0864c23e36e2812a934d5e39b9e9230
Kangkang <l991018000423@163.com>
2023-02-25 18:15:17 +0800

    抽象类

---
+++ 009-抽象类/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _009_抽象类
+{
+    abstract class Enemy
+    {
+        public abstract void Attack();//抽象类有抽象方法,而抽象方法没有函数体
+    }
+}
