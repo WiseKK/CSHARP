cf971ff7fa15b8de7e50b722751b830bc29a3db2
WiseKK <127091057+WiseKK@users.noreply.github.com>
2023-04-11 13:48:48 +0800

    一些方法

---
+++ TEST/随机数/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 随机数
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+                Random random = new Random();
+                string name = "";
+
+                for (int i = 0; i < 5; i++)
+                {
+                    name += random.Next(0, 10).ToString();
+                }
+
+                Console.WriteLine("随机生成的五位数名字为：" + name);
+                Console.ReadLine();         
+        }
+    }
+}
--- 第四季-高级篇/03-字符串/Program.cs
+++ 第四季-高级篇/03-字符串/Program.cs
@@ -10,14 +10,49 @@ namespace _03_字符串
     {
         static void Main(string[] args)
         {
-            string s = "kangkang";
-            Console.WriteLine(s.Length);
-            Console.WriteLine(s=="kangkang");
-            s = "http://" + s;
+            string s = "Kangkang";//创建字符串
+            Console.WriteLine(s.Length);//获取，输出字符串长度
+            Console.WriteLine(s=="kangkang  ");//判断是否相同
+            s = "  http://" + s;//字符串链接
             Console.WriteLine(s);
             Console.WriteLine(s[3]);
             Console.WriteLine(s.Length);
 
+
+            //更多字符串代码
+            Console.WriteLine(s.CompareTo("kangkang"));//对比
+            Console.WriteLine(s.Replace("k","l"));//替换    
+            Console.WriteLine(s.Split());//拆分            
+            # region  遍历拆分结果          
+            string[] vs = s.Split(s[3]);
+            foreach (string v in vs)
+            {
+                Console.WriteLine(v);
+            }
+            #endregion
+            Console.WriteLine(s.Substring(5));//截取
+            Console.WriteLine(s.Substring(4, 4));//从那里开始，截取几个
+            Console.WriteLine(s.ToLower());//小写
+            Console.WriteLine(s.ToUpper());//大写
+            Console.WriteLine(s.Trim());//去头尾空格
+            Console.WriteLine(string.Concat("123456","cadcdac"));//相加
+
+            //char[] cA = new char[20];//字符串拷贝到（不清楚）
+            //s.CopyTo(4,cA,1,8);
+            //#region  遍历拆分结果          
+            //foreach (char c in cA)
+            //{
+            //    Console.WriteLine(c);
+            //}
+            //#endregion
+
+
+
+
+
+
+
         }
+
     }
 }
