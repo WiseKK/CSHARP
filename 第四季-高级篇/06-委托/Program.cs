98c1a5abf201e6f012592636b685634e666af8ac
WiseKK <127091057+WiseKK@users.noreply.github.com>
2023-04-13 17:48:36 +0800

    委托（部分）

---
+++ 第四季-高级篇/06-委托/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_委托
+{
+    internal class Program
+    {
+        delegate void IntMethodInvoker(int x);
+        delegate long TwoLong(long a, long b);
+        static void Main(string[] args)
+        {
+            IntMethodInvoker invoker = null;
+            TwoLong invoker2 = null;
+
+            invoker = Program.test;//委托给invoker
+            invoker(100);
+
+            if (invoker2 != null)//防止为空出错
+            {
+                invoker2(2, 23);
+            }
+        }
+        private static void test(int x)
+        {
+            Console.WriteLine("我是test方法："+ x);
+        }
+    }
+}
